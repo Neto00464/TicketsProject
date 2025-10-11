ae73131cf64014a6d6498dc807257a701a3586c9
Ernesto Gomez <jgomez00464@ufide.ac.cr>
2025-10-10 21:46:26 -0600

    Esta es una prueba numero 2

--- src/main/java/com/mycompany/tcketsproject/TcketsProject.java
+++ src/main/java/com/mycompany/tcketsproject/TcketsProject.java
@@ -7,5 +7,6 @@ public class TcketsProject {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Pueba de regreso al PC");
+        System.out.println("Segunda prueba a la PC");
     }
 }
