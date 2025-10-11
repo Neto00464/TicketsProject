5fe8eee073ece79681f0cf3ca01600d79a849a67
Neto00464 <123911907+Neto00464@users.noreply.github.com>
2025-10-10 21:42:13 -0600

    Update TcketsProject.java

--- src/main/java/com/mycompany/tcketsproject/TcketsProject.java
+++ src/main/java/com/mycompany/tcketsproject/TcketsProject.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.tcketsproject;
 
-/**
- *
- * @author Neto
- */
 public class TcketsProject {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Pueba de regreso al PC");
     }
 }
