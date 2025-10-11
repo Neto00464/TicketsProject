2dd3d1020cc99fb6036bdcfdd8f7c3e0945c4f1f
Ernesto Gomez <jgomez00464@ufide.ac.cr>
2025-10-10 21:41:08 -0600

    Esta es una prueba

---
+++ src/main/java/com/mycompany/tcketsproject/TcketsProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tcketsproject;
+
+/**
+ *
+ * @author Neto
+ */
+public class TcketsProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
