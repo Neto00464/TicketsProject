ee78c146b5e7537621258c7dfa25c70512aede4f
Ernesto Gomez <jgomez00464@ufide.ac.cr>
2025-10-19 13:34:00 -0600

    Commit 1 Jose Gomez

--- src/main/java/com/mycompany/tcketsproject/TcketsProject.java
+++ src/main/java/com/mycompany/tcketsproject/TcketsProject.java
@@ -5,10 +5,6 @@ package com.mycompany.tcketsproject;
 public class TcketsProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Pueba de regreso al PC");
-        System.out.println("Segunda prueba a la PC");
-        System.out.println("Pueba de regreso al PC#2");
-        System.out.println("Prueba del lunes");
+        
     }
 }
