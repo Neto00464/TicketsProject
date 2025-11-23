5f318fd173240b89bf02bc35f5346931d8ee9b3d
Ernesto Gomez <jgomez00464@ufide.ac.cr>
2025-11-23 14:20:12 -0600

    Jose Gomez | Clorregir clases, errores de sintaxis

--- src/main/java/com/mycompany/tcketsproject/Mensaje.java
+++ src/main/java/com/mycompany/tcketsproject/Mensaje.java
@@ -5,19 +5,19 @@ import java.time.LocalDateTime;
 
 
 public class Mensaje {
-private Usuario autor;
-private String contenido;
-private LocalDateTime fecha;
+    private Usuario autor;
+    private String contenido;
+    private LocalDateTime fecha;
 
 
-public Mensaje(Usuario autor, String contenido) {
-this.autor = autor;
-this.contenido = contenido;
-this.fecha = LocalDateTime.now();
-}
+    public Mensaje(Usuario autor, String contenido) {
+        this.autor = autor;
+        this.contenido = contenido;
+        this.fecha = LocalDateTime.now();
+    }
 
 
-public Usuario getAutor() { return autor; }
-public String getContenido() { return contenido; }
-public LocalDateTime getFecha() { return fecha; }
+    public Usuario getAutor() { return autor; }
+    public String getContenido() { return contenido; }
+    public LocalDateTime getFecha() { return fecha; }
 }
