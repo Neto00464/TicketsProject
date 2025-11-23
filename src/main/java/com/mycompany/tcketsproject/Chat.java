bcf179f924c66417b9ba43c1ca2917845dd2c0a0
Ernesto Gomez <jgomez00464@ufide.ac.cr>
2025-11-23 14:12:46 -0600

    Jose Gomez | Ceacion de la clase chat

---
+++ src/main/java/com/mycompany/tcketsproject/Chat.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tcketsproject;
+
+import java.util.ArrayList;
+
+
+public class Chat {
+private ArrayList<Mensaje> mensajes;
+
+
+public Chat() {
+this.mensajes = new ArrayList<>();
+}
+
+
+public void agregarMensaje(Mensaje m) {
+mensajes.add(m);
+}
+
+
+public ArrayList<Mensaje> getMensajes() {
+return mensajes;
